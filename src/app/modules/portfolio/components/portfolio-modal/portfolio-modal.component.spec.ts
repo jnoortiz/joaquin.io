d88af9a1c2ea42cba7d3d28dd090be5223df1881
Joaquin Ortiz <jno.ortiz@gmail.com>
2021-01-24 18:25:50 +0800

    Upgrade to Angular 11

--- src/app/modules/portfolio/components/portfolio-modal/portfolio-modal.component.spec.ts
+++ src/app/modules/portfolio/components/portfolio-modal/portfolio-modal.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PortfolioModalComponent } from './portfolio-modal.component';
 
@@ -6,7 +6,7 @@ describe('PortfolioModalComponent', () => {
   let component: PortfolioModalComponent;
   let fixture: ComponentFixture<PortfolioModalComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PortfolioModalComponent ]
     })
