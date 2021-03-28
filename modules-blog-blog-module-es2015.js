2acf60dce24224507dc1131e84d0bca03cedc725
Joaquin Ortiz <jno.ortiz@gmail.com>
2021-03-28 15:55:55 +0800

    build folder

---
+++ modules-blog-blog-module-es2015.js
@@ -0,0 +1,30 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["modules-blog-blog-module"],{
+
+/***/ "X6tI":
+/*!*********************************************!*\
+  !*** ./src/app/modules/blog/blog.module.ts ***!
+  \*********************************************/
+/*! exports provided: BlogModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "BlogModule", function() { return BlogModule; });
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/common */ "ofXK");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+class BlogModule {
+}
+BlogModule.ɵfac = function BlogModule_Factory(t) { return new (t || BlogModule)(); };
+BlogModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineNgModule"]({ type: BlogModule });
+BlogModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineInjector"]({ imports: [[
+            _angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"]
+        ]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵsetNgModuleScope"](BlogModule, { imports: [_angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"]] }); })();
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=modules-blog-blog-module-es2015.js.map
